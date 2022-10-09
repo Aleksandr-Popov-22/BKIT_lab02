6d9cba0278ca087d564a2b39761e9498d617408b
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2022-10-09 23:23:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from lab_python_oop.square import Square
 
 
 def main():
-    r = Rectangle("синего", 3, 2)
-    c = Circle("зеленого", 5)
-    s = Square("красного", 5)
+    r = Rectangle("синего", 17, 17)
+    c = Circle("зеленого", 17)
+    s = Square("красного", 17)
     print(r)
     print(c)
     print(s)
